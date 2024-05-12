5f4732e5c9e9e5e04557adfd83b6435d37b6d824
Khang Tran <khangtrann8198@gmail.com>
2024-05-12 23:44:21 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CurtainsJsDirective } from './curtainsjs.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+    <div appCurtainsJs>
+      <div id="page-wrap">
+        <div id="canvas"></div>
+        <div class="curtain-wrapper">
+          <div class="curtain">
+            <video src="0512.mov" crossorigin="" data-sampler="simplePlaneVideoTexture"></video>
+          </div>
+        </div>
+      </div>
+    </div>
+  `,
+  styleUrl: './app.component.scss',
+  imports: [CurtainsJsDirective],
 })
-export class AppComponent {
-  title = 'curtainsjs-showcase';
-}
+export class AppComponent {}
